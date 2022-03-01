e327ffb8ae278881db588b3cf66366003e117b03
JeongHoLim <9997ijh@gmail.com>
2022-03-01 18:37:51 +0900

    .

--- src/main/java/com/ari/sogang/SogangApplication.java
+++ src/main/java/com/ari/sogang/SogangApplication.java
@@ -2,6 +2,7 @@ package com.ari.sogang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,11 +14,12 @@ public class SogangApplication {
         SpringApplication.run(SogangApplication.class, args);
     }
 
+    @Bean
     public WebMvcConfigurer corsConfigurer(){
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("http://localhost:3000");
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
             }
         };
     }
