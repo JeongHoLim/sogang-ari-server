6cd4b628246803fc9d2a272521f2828eb829c2a1
JeongHoLim <9997ijh@gmail.com>
2022-03-07 01:06:20 +0900

    fix : loginResponseDto 오류 수정

--- src/main/java/com/ari/sogang/config/dto/LoginResponseDto.java
+++ src/main/java/com/ari/sogang/config/dto/LoginResponseDto.java
@@ -5,11 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Component
 public class LoginResponseDto {
     UserDto userInfo;
     TokenDto tokenInfo;
