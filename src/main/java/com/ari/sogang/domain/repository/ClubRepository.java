4cb7cce3a46948f875a8c27df4c1405c70679fbf
JeongHoLim <9997ijh@gmail.com>
2022-03-18 04:51:19 +0900

    feat : 페이지 검색 기능 추가

--- src/main/java/com/ari/sogang/domain/repository/ClubRepository.java
+++ src/main/java/com/ari/sogang/domain/repository/ClubRepository.java
@@ -1,17 +1,20 @@
 package com.ari.sogang.domain.repository;
 
 import com.ari.sogang.domain.entity.Club;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ClubRepository extends JpaRepository<Club,Long> {
 
-    Club findByName(String name);
-
     List<Club> findAllBySection(String section);
 
     List<Club> findByNameContains(String name);
 
+    Page<Club> findAllBySection(String section,Pageable pageable);
+
+    Page<Club> findByNameContains(String name,Pageable pageable);
 
 }
